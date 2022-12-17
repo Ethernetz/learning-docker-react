391927644c1f5e2514fba6b31f08484a417af588
Ethan Netz <ethannetz@hotmail.com>
2022-12-17 11:33:24 -0500

    test new message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          123456789
+          Welcomme to CSC 280!
         </p>
         <a
           className="App-link"
